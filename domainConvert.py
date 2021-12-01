8c4792dc17887946a72a4fbdd31cfb461a1b478b
foordeluxe <68303704+foordeluxe@users.noreply.github.com>
2021-12-01 19:48:27 +0100

    Domain converter to rule
    Domain converter to rule

---
+++ domainConvert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# This tool is creating from emaildomain.com, to rules.
+# Usage: ./domainConvert.py file.txt output.rule
+import sys
+
+with open(sys.argv[1], 'r') as passFile:
+    passList = passFile.read().splitlines()
+
+for line in passList:
+    val = ""
+    incr = 0
+    for c in line.rstrip():
+        if incr == 0:
+            val = "$@"
+        else:
+            val += "$" + c
+        incr += 1
+    with open(sys.argv[2], "a") as myFile:
+        myFile.write(val + "\n")

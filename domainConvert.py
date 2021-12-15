8532cb63cb2d97a4da4d161b8d540acb2f51fd29
foordeluxe <68303704+foordeluxe@users.noreply.github.com>
2021-12-15 01:23:13 +0100

    Updated domain to rule

--- domainConvert.py
+++ domainConvert.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 # This tool is creating from emaildomain.com, to rules.
 # Usage: ./domainConvert.py file.txt output.rule
+
 import sys
+import os.path
 
-with open(sys.argv[1], 'r') as passFile:
-    passList = passFile.read().splitlines()
-
-for line in passList:
-    val = ""
-    incr = 0
-    for c in line.rstrip():
-        if incr == 0 and c != "@":
-            val = "$@"
-        else:
-            val += "$" + c
-        incr += 1
-    with open(sys.argv[2], "a") as myFile:
-        myFile.write(val + "\n")
+if len(sys.argv) == 3:
+    if os.path.isfile(sys.argv[1]):
+        with open(sys.argv[1], 'r') as passFile:
+            passList = passFile.read().splitlines()
+            for line in passList:
+                val = ""
+                incr = 0
+                for c in line.rstrip():
+                    if incr == 0 and c != '@':
+                        val = "$@"
+                    else:
+                        val += "$" + c
+                    incr += 1
+                with open(sys.argv[2], "a") as myFile:
+                    myFile.write(val + "\n")
+    else:
+        print("File not found")
+else:
+    print("Not enough parameters")
